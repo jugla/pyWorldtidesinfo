bd93e0ed2eeb6fc6ff706548abf16e7d2f259ccf
jugla <jugla@free.fr>
2021-12-05 01:08:01 +0100

    first release

---
+++ tests/test_client.py
@@ -0,0 +1,15 @@
+
+class WorldTidesInfoTestCase(unittest.TestCase):
+    """Class used to test."""
+
+    def test(self):
+        """Test method."""
+        data = None
+        if not data:
+            assert True
+        else:
+            assert False
+
+
+if __name__ == "__main__":
+    unittest.main()
